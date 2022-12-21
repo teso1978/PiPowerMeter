4522ff0af4ce7ce7ba81f2ae9a0bee18144e5578
Hajdu István <53936306+teso1978@users.noreply.github.com>
2022-12-21 16:01:50 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install 15
+nvm install 16
 sudo cp -R $NVM_DIR/versions/node/$(nvm version)/* /usr/local/
 
 # install git
