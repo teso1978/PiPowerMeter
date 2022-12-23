5fd954446534aab2efdf74d8f7550b16386fd2e0
Hajdu István <53936306+teso1978@users.noreply.github.com>
2022-12-23 23:10:43 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,4 +17,16 @@ else
 	sudo mv ~/app/.bash_aliases ~
 	echo "installed .bash_aliases"
 fi
+echo '>>> Enable UART'
+
+sudo raspi-config nonint do_serial 2
+
+# Bluetooth uses the primary uart so it needs to be disabled
+# Only applies to RPi's with built-in Bluetooth (3A+, 3B, 3B+, 4B and Zero W)
+if grep -qE "^Revision\s*:\s*[ 123][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F](08|0c|0d|0e|11)[0-9a-fA-F]$" /proc/cpuinfo; then
+	echo '>>> Disable bluetooth'
+	sudo raspi-config nonint set_config_var dtoverlay pi3-disable-bt /boot/config.txt
+	sudo systemctl disable hciuart
+fi
+
 echo "finished installing PiPowerMeter" 
