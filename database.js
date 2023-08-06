1c9ada3afcf13233f67ca0590b86f3d9bfb0120c
Hajdu István <53936306+teso1978@users.noreply.github.com>
2023-08-06 18:50:07 +0200

    Update database.js

--- database.js
+++ database.js
@@ -118,7 +118,7 @@ var powerDb = new sqlite3.Database(databaseFile, function (err) {
 		    
 		db.runSql("Insert or ignore into Config Values('VoltageScale', '380');", null, true);
 		    
-                db.runSql("Insert or ignore into Config Values('Probes', '[{''Name'':''SCT-006'',''Factor'':''40''},{''Name'':''SCT-010'',''Factor'':''150''},{''Name'':''SCT-013'',''Factor'':''100''},{''Name'':''SCT-016'',''Factor'':''150''},{''Name'':''SCT-019'',''Factor'':''300''},{"Name":"ZMCT103C","Factor":"25"}]');", null, true);
+                db.runSql("Insert or ignore into Config Values('Probes', '[{''Name'':''SCT-006'',''Factor'':''40''},{''Name'':''SCT-010'',''Factor'':''150''},{''Name'':''SCT-013'',''Factor'':''100''},{''Name'':''SCT-016'',''Factor'':''150''},{''Name'':''SCT-019'',''Factor'':''300''},{''Name'':''ZMCT103C'',''Factor'':''25''}]');", null, true);
 
                 console.log('Config table ready');
                 TableStates.Config = true;
